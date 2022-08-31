c1074a7a85f09c391a11a42c1cdcd5bc426aee09
Diogo Danilo <52755892+DiogoD31@users.noreply.github.com>
2022-08-31 13:21:33 -0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link"
+
+export default function FAQPage() {
+    return(
+        <div>
+            <h1>Você se perdeu :O</h1>
+            <Link href="/">
+                Ir para a página home
+            </Link>
+        </div>
+    )
+}
